2b8f3b60cbc199cabdedd2f8344e7a115005bca6
Jan Arnold <jan.arnold@coraxx.net>
2016-02-23 17:03:13 +0100

    - finished drag and drop sorting (updating qgraphicitems etc.) - custom qt functions now in seperate file

--- QtCustom.py
+++ QtCustom.py
@@ -1,29 +1,56 @@
 from PyQt4 import QtCore, QtGui
+import clrmsg
+##############################
+## QTableViewCustom
 
 class QTableViewCustom(QtGui.QTableView):
 	def __init__(self, parent=None):
 		QtGui.QTableView.__init__(self,parent)
 		self.parent = parent
+		self.debug = True
+		self._drop = False
 
-	def mousePressEvent(self,event):
-		print "yeay"
-		super(QTableViewCustom, self).mousePressEvent(event)
+		## Enable Drag'n'Drop
+		self.setDragDropOverwriteMode(False)
+		self.setDragEnabled(True)
+		self.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
 
-	def contextMenuEvent(self, event):
-		self.menu = QtGui.QMenu(self)
-		renameAction = QtGui.QAction('Rename', self)
-		renameAction.triggered.connect(self.renameSlot)
-		self.menu.addAction(renameAction)
-		# add other required actions
-		self.menu.popup(QtGui.QCursor.pos())
+	def mouseMoveEvent(self,event):
+		super(QTableViewCustom, self).mouseMoveEvent(event)
+		## Drop Flag to trigger item update only when there was a row move
+		if self._drop == True:
+			self.updateItems()
+			self._drop = False
 
-	def renameSlot(self):
-		print "renaming slot called"
+	def dropEvent(self,event):
+		## Drop Flag to trigger item update only when there was a row move
+		self._drop = True
+		super(QTableViewCustom, self).dropEvent(event)
 
+	# def contextMenuEvent(self, event):
+	# 	self.menu = QtGui.QMenu(self)
+	# 	renameAction = QtGui.QAction('Rename', self)
+	# 	renameAction.triggered.connect(self.renameSlot)
+	# 	self.menu.addAction(renameAction)
+	# 	# add other required actions
+	# 	self.menu.popup(QtGui.QCursor.pos())
+
+	# def renameSlot(self):
+	# 	print "renaming slot called"
+
+												###############################################
+												#######          Update items           #######
+												#################### START ####################
 	def updateItems(self):
-		row = 0
+		items = []
 		for item in self._scene.items():
 			if isinstance(item, QtGui.QGraphicsEllipseItem):
+				items.append(item)
+		if self.debug == True: print clrmsg.DEBUG + "Update items check - Nr. of items/rows:", len(items), self._model.rowCount()
+		if len(items) == self._model.rowCount():
+			row = 0
+			for item in items:
+				if self.debug == True: print clrmsg.DEBUG + 'Row:', row, '|', self._model.data(self._model.index(row, 0)).toString(),self._model.data(self._model.index(row, 1)).toString()
 				item.setPos(float(self._model.data(self._model.index(row, 0)).toString()),float(self._model.data(self._model.index(row, 1)).toString()))
 				row += 1
 
@@ -59,4 +86,159 @@ class QTableViewCustom(QtGui.QTableView):
 				self._scene.removeItem(activeitems[row])
 				self._scene.enumeratePoints()
 			self._scene.itemsToModel()
+												##################### END #####################
+												#######          Update items           #######
+												###############################################
+
+##############################
+## QStandardItemModelCustom
+
+class QStandardItemModelCustom(QtGui.QStandardItemModel):
+	def __init__(self, parent=None):
+		QtGui.QStandardItemModel.__init__(self,parent)
+		self.parent = parent
+
+	def dropMimeData(self,data,action,row,column,parent):
+		return QtGui.QStandardItemModel.dropMimeData(self,data,action,row,0,parent)
+
+
+##############################
+## QGraphicsSceneCustom
+
+class QGraphicsSceneCustom(QtGui.QGraphicsScene):
+	def __init__(self, parent=None,name=None,model=None):
+		QtGui.QGraphicsScene.__init__(self,parent)
+		self.parent = parent
+		self.name = name
+		self.model = model
+		self.parent.setDragMode(QtGui.QGraphicsView.NoDrag)
+		## set standard pen color
+		self.pen = QtGui.QPen(QtCore.Qt.red)
+		self.lastScreenPos = QtCore.QPoint(0, 0)
+		self.lastScenePos = 0
+		self.selectionmode = False
+		self.pointidx = 1
+		## Circle size
+		self.cs = 20
+
+	def wheelEvent(self, event):
+		## Scaling
+		if event.delta() > 0:
+			scalingFactor = 1.15
+		else:
+			scalingFactor = 1 / 1.15
+		self.parent.scale(scalingFactor, scalingFactor)
+		## Center on mouse pos only if mouse moved mor then 25px
+		if (event.screenPos()-self.lastScreenPos).manhattanLength() > 25:
+			self.parent.centerOn(event.scenePos().x(), event.scenePos().y())
+			self.lastScenePos = event.scenePos()
+		else:
+			self.parent.centerOn(self.lastScenePos.x(), self.lastScenePos.y())
+		## Save pos for precise scrolling, i.e. centering view only when mouse moved
+		self.lastScreenPos = event.screenPos()
+
+	def mousePressEvent(self, event):
+		modifiers = QtGui.QApplication.keyboardModifiers()
+		if event.button() == QtCore.Qt.LeftButton and modifiers != QtCore.Qt.ControlModifier:
+			self.parent.setDragMode(QtGui.QGraphicsView.ScrollHandDrag)
+			## Model does not to be refreshed every time while navigating
+			return
+		elif event.button() == QtCore.Qt.LeftButton and modifiers == QtCore.Qt.ControlModifier:
+			self.parent.setDragMode(QtGui.QGraphicsView.RubberBandDrag)
+			self.selectionmode = True
+			## Model does not to be refreshed every time while selecting
+			return
+		elif event.button() == QtCore.Qt.RightButton:
+			## First add at 0,0 then move to get position from item.scenePos() or .x() and y.()
+			circle = self.addEllipse(-self.cs/2, -self.cs/2, self.cs, self.cs, self.pen)
+			circle.setPos(event.scenePos().x(), event.scenePos().y())
+			circle.setFlag(QtGui.QGraphicsItem.ItemIsMovable, True)
+			circle.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
+			## Reorder to have them in ascending order in the tableview
+			QtGui.QGraphicsItem.stackBefore(circle, self.items()[-2])
+			self.enumeratePoints()
+			#self.addPointToModel(event.scenePos().x(), event.scenePos().y())
+		elif event.button() == QtCore.Qt.MiddleButton:
+			item = self.itemAt(event.scenePos())
+			if isinstance(item, QtGui.QGraphicsEllipseItem):
+				#print item.mapToScene(0, 0).x(),item.mapToScene(0, 0).y()
+				# print item.x(),item.y()
+				# item.setPos(100.0,100.0)
+				# print item.x(),item.y()
+				self.removeItem(item)
+				self.enumeratePoints()
+		self.itemsToModel()
+
+	def mouseReleaseEvent(self, event):
+		## Reinitialize mouseReleaseEvent handling from QtGui.QGraphicsScene for item drag and drop feature
+		super(QGraphicsSceneCustom, self).mouseReleaseEvent(event)
+		## Only update position when single item is drag and dropped
+		if self.selectedItems() and self.selectionmode == False:
+			#print 'New pos:', self.selectedItems()[0].x(), self.selectedItems()[0].y()
+			self.clearSelection()
+			self.itemsToModel()
+		self.parent.setDragMode(QtGui.QGraphicsView.NoDrag)
+		self.selectionmode = False
+
+	def keyPressEvent(self, event):
+		if event.key() == QtCore.Qt.Key_Delete:
+			for item in self.selectedItems(): self.removeItem(item)
+			self.itemsToModel()
+
+	def enumeratePoints(self):
+		## Remove numbering
+		for item in self.items():
+			if isinstance(item, QtGui.QGraphicsSimpleTextItem) or isinstance(item, QtGui.QGraphicsLineItem):
+				self.removeItem(item)
+		pointidx = 1
+		for item in self.items():
+			if isinstance(item, QtGui.QGraphicsEllipseItem):
+				nr = self.addSimpleText(str(pointidx),QtGui.QFont("Helvetica", pointSize = self.cs))
+				nr.setParentItem(item)
+				nr.setPos(self.cs/2,self.cs/4)
+				#nr.setPen(self.pen) # outline
+				nr.setBrush(QtCore.Qt.cyan) # fill
+				## Adding crosshair
+				hline = self.addLine(-self.cs/2-2,0,self.cs/2+2,0)
+				hline.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255, 128)))# r,g,b,alpha
+				hline.setParentItem(item)
+				vline = self.addLine(0,-self.cs/2-2,0,self.cs/2+2)
+				vline.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255, 128)))# r,g,b,alpha
+				vline.setParentItem(item)
+				## Counter
+				pointidx += 1
+
+	def itemsToModel(self):
+		self.model.removeRows(0,self.model.rowCount())
+		for item in self.items():
+			if isinstance(item, QtGui.QGraphicsEllipseItem):
+				x_item = QtGui.QStandardItem(str(item.x()))
+				y_item = QtGui.QStandardItem(str(item.y()))
+				x_item.setFlags(x_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+				y_item.setFlags(y_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+				items = [x_item, y_item]
+				self.model.appendRow(items)
+				self.model.setHeaderData(0, QtCore.Qt.Horizontal,'x')
+				self.model.setHeaderData(1, QtCore.Qt.Horizontal,'y')
+
+	# def addPointToModel(self,x,y):
+	# 	x_item = QtGui.QStandardItem(str(x))
+	# 	y_item = QtGui.QStandardItem(str(y))
+	# 	x_item.setFlags(x_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+	# 	y_item.setFlags(y_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
+	# 	items = [x_item, y_item]
+	# 	model = getattr(widget, "%s" % "model_"+self.name)
+	# 	model.appendRow(items)
+	# 	model.setHeaderData(0, QtCore.Qt.Horizontal,'x')
+	# 	model.setHeaderData(1, QtCore.Qt.Horizontal,'y')
+
+# class StandardItemModelHandler(QtGui.QStandardItemModel):
+# 	def __init__(self, parent=None):
+# 		QtGui.QStandardItemModel.__init__(self,parent)
+# 		self.parent = parent
+
+# 	def mousePressEvent(self, event):
+# 		print 'click from', self.objectName()
+
+
 
--- correlation_widget.py
+++ correlation_widget.py
@@ -21,147 +21,13 @@ import cv2
 import tifffile as tf
 ## Colored stdout
 import clrmsg
+import QtCustom
 
 execdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(execdir)
 
 qtCreatorFile_main =  os.path.join(execdir, "TDCT_correlation.ui")
 Ui_WidgetWindow, QtBaseClass = uic.loadUiType(qtCreatorFile_main)
-
-class QGraphicsSceneCustom(QtGui.QGraphicsScene):
-	def __init__(self, parent=None,name=None,model=None):
-		QtGui.QGraphicsScene.__init__(self,parent)
-		self.parent = parent
-		self.name = name
-		self.model = model
-		self.parent.setDragMode(QtGui.QGraphicsView.NoDrag)
-		## set standard pen color
-		self.pen = QtGui.QPen(QtCore.Qt.red)
-		self.lastScreenPos = QtCore.QPoint(0, 0)
-		self.lastScenePos = 0
-		self.selectionmode = False
-		self.pointidx = 1
-		## Circle size
-		self.cs = 10
-
-	def wheelEvent(self, event):
-		## Scaling
-		if event.delta() > 0:
-			scalingFactor = 1.15
-		else:
-			scalingFactor = 1 / 1.15
-		self.parent.scale(scalingFactor, scalingFactor)
-		## Center on mouse pos only if mouse moved mor then 25px
-		if (event.screenPos()-self.lastScreenPos).manhattanLength() > 25:
-			self.parent.centerOn(event.scenePos().x(), event.scenePos().y())
-			self.lastScenePos = event.scenePos()
-		else:
-			self.parent.centerOn(self.lastScenePos.x(), self.lastScenePos.y())
-		## Save pos for precise scrolling, i.e. centering view only when mouse moved
-		self.lastScreenPos = event.screenPos()
-
-	def mousePressEvent(self, event):
-		modifiers = QtGui.QApplication.keyboardModifiers()
-		if event.button() == QtCore.Qt.LeftButton and modifiers != QtCore.Qt.ControlModifier:
-			self.parent.setDragMode(QtGui.QGraphicsView.ScrollHandDrag)
-			## Model does not to be refreshed every time while navigating
-			return
-		elif event.button() == QtCore.Qt.LeftButton and modifiers == QtCore.Qt.ControlModifier:
-			self.parent.setDragMode(QtGui.QGraphicsView.RubberBandDrag)
-			self.selectionmode = True
-			## Model does not to be refreshed every time while selecting
-			return
-		elif event.button() == QtCore.Qt.RightButton:
-			## First add at 0,0 then move to get position from item.scenePos() or .x() and y.()
-			circle = self.addEllipse(-self.cs/2, -self.cs/2, self.cs, self.cs, self.pen)
-			circle.setPos(event.scenePos().x(), event.scenePos().y())
-			circle.setFlag(QtGui.QGraphicsItem.ItemIsMovable, True)
-			circle.setFlag(QtGui.QGraphicsItem.ItemIsSelectable, True)
-			## Reorder to have them in ascending order in the tableview
-			QtGui.QGraphicsItem.stackBefore(circle, self.items()[-2])
-			self.enumeratePoints()
-			#self.addPointToModel(event.scenePos().x(), event.scenePos().y())
-		elif event.button() == QtCore.Qt.MiddleButton:
-			item = self.itemAt(event.scenePos())
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				#print item.mapToScene(0, 0).x(),item.mapToScene(0, 0).y()
-				# print item.x(),item.y()
-				# item.setPos(100.0,100.0)
-				# print item.x(),item.y()
-				self.removeItem(item)
-				self.enumeratePoints()
-		self.itemsToModel()
-
-	def mouseReleaseEvent(self, event):
-		## Reinitialize mouseReleaseEvent handling from QtGui.QGraphicsScene for item drag and drop feature
-		super(QGraphicsSceneCustom, self).mouseReleaseEvent(event)
-		## Only update position when single item is drag and dropped
-		if self.selectedItems() and self.selectionmode == False:
-			#print 'New pos:', self.selectedItems()[0].x(), self.selectedItems()[0].y()
-			self.clearSelection()
-			self.itemsToModel()
-		self.parent.setDragMode(QtGui.QGraphicsView.NoDrag)
-		self.selectionmode = False
-
-	def keyPressEvent(self, event):
-		if event.key() == QtCore.Qt.Key_Delete:
-			for item in self.selectedItems(): self.removeItem(item)
-			self.itemsToModel()
-
-	def enumeratePoints(self):
-		## Remove numbering
-		for item in self.items():
-			if isinstance(item, QtGui.QGraphicsSimpleTextItem) or isinstance(item, QtGui.QGraphicsLineItem):
-				self.removeItem(item)
-		pointidx = 1
-		for item in self.items():
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				nr = self.addSimpleText(str(pointidx),QtGui.QFont("Helvetica", pointSize = self.cs))
-				nr.setParentItem(item)
-				nr.setPos(self.cs/2,self.cs/4)
-				#nr.setPen(self.pen) # outline
-				nr.setBrush(QtCore.Qt.cyan) # fill
-				## Adding crosshair
-				hline = self.addLine(-self.cs/2-2,0,self.cs/2+2,0)
-				hline.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255, 128)))# r,g,b,alpha
-				hline.setParentItem(item)
-				vline = self.addLine(0,-self.cs/2-2,0,self.cs/2+2)
-				vline.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255, 128)))# r,g,b,alpha
-				vline.setParentItem(item)
-				## Counter
-				pointidx += 1
-
-	def itemsToModel(self):
-		self.model.removeRows(0,self.model.rowCount())
-		for item in self.items():
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				x_item = QtGui.QStandardItem(str(item.x()))
-				y_item = QtGui.QStandardItem(str(item.y()))
-				x_item.setFlags(x_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
-				y_item.setFlags(y_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
-				items = [x_item, y_item]
-				self.model.appendRow(items)
-				self.model.setHeaderData(0, QtCore.Qt.Horizontal,'x')
-				self.model.setHeaderData(1, QtCore.Qt.Horizontal,'y')
-
-	# def addPointToModel(self,x,y):
-	# 	x_item = QtGui.QStandardItem(str(x))
-	# 	y_item = QtGui.QStandardItem(str(y))
-	# 	x_item.setFlags(x_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
-	# 	y_item.setFlags(y_item.flags() & ~QtCore.Qt.ItemIsDropEnabled)
-	# 	items = [x_item, y_item]
-	# 	model = getattr(widget, "%s" % "model_"+self.name)
-	# 	model.appendRow(items)
-	# 	model.setHeaderData(0, QtCore.Qt.Horizontal,'x')
-	# 	model.setHeaderData(1, QtCore.Qt.Horizontal,'y')
-
-# class StandardItemModelHandler(QtGui.QStandardItemModel):
-# 	def __init__(self, parent=None):
-# 		QtGui.QStandardItemModel.__init__(self,parent)
-# 		self.parent = parent
-
-# 	def mousePressEvent(self, event):
-# 		print 'click from', self.objectName()
 
 class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 	def __init__(self, parent=None, left=None, right=None):
@@ -171,21 +37,12 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 		self.debug = True
 
 		## Tableview and models
-		print dir(self.tableView_left.model)
-		self.model_left = QtGui.QStandardItemModel(self)
-		print dir(self.model_left)
+		self.model_left = QtCustom.QStandardItemModelCustom(self)
 		self.tableView_left.setModel(self.model_left)
-		self.model_right = QtGui.QStandardItemModel(self)
+		self.model_left.tableview = self.tableView_left
+		self.model_right = QtCustom.QStandardItemModelCustom(self)
 		self.tableView_right.setModel(self.model_right)
-
-		self.tableView_left.setDragDropOverwriteMode(False)
-		self.tableView_left.setDragEnabled(True)
-		self.tableView_left.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
-		self.tableView_right.setDragDropOverwriteMode(False)
-		self.tableView_right.setDragEnabled(True)
-		self.tableView_right.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
-
-		print dir(self.tableView_left.model)
+		self.model_right.tableview = self.tableView_right
 
 		## store parameters for resizing
 		self.parent = parent
@@ -204,20 +61,11 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 		self.initImageLeft()
 		self.initImageRight()
 
-		## connect item change signal to write changes in model back to QGraphicItems
-		# self.model_left.itemChanged.connect(lambda: self.updateItems(self.model_left,self.sceneLeft))
-		# self.model_right.itemChanged.connect(lambda: self.updateItems(self.model_right,self.sceneRight))
-
+		## connect item change signal to write changes in model back to QGraphicItems as well as highlighting selected points
 		self.model_left.itemChanged.connect(self.tableView_left.updateItems)
 		self.model_right.itemChanged.connect(self.tableView_right.updateItems)
-
-		# self.tableView_left.selectionModel().selectionChanged.connect(lambda: self.showSelectedItem(self.tableView_left,self.sceneLeft))
-		# self.tableView_right.selectionModel().selectionChanged.connect(lambda: self.showSelectedItem(self.tableView_right,self.sceneRight))
 		self.tableView_left.selectionModel().selectionChanged.connect(self.tableView_left.showSelectedItem)
 		self.tableView_right.selectionModel().selectionChanged.connect(self.tableView_right.showSelectedItem)
-
-		#self.tableView_left.doubleClicked.connect(lambda: self.deleteItem(self.tableView_right,self.model_right,self.sceneRight))
-		#keyPressEvent(QtCore.Qt.Key_Delete)
 
 		# SpinBoxes
 		self.spinBox_rot.valueChanged.connect(self.rotateImage)
@@ -309,13 +157,13 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 		self.toolButton_rotcw.setEnabled(status)
 		self.toolButton_rotccw.setEnabled(status)
 
-												#################################################
-												####### Image initialization and rotation #######
-																	## START ##
+												###############################################
+												###### Image initialization and rotation ######
+												#################### START ####################
 	def initImageLeft(self):
 		if self.left != None:
-			## Changed GraphicsSceneLeft(self) to QGraphicsSceneCustom(self.graphicsView_left) to reuse class for both scenes
-			self.sceneLeft = QGraphicsSceneCustom(self.graphicsView_left,name='left',model=self.model_left)
+			## Changed GraphicsSceneLeft(self) to QtCustom.QGraphicsSceneCustom(self.graphicsView_left) to reuse class for both scenes
+			self.sceneLeft = QtCustom.QGraphicsSceneCustom(self.graphicsView_left,name='left',model=self.model_left)
 			## Load image and assign to scene
 			self.img_left = self.imread(self.left)
 			#self.pixmap_left = QtGui.QPixmap(self.left)
@@ -331,7 +179,7 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 
 	def initImageRight(self):
 		if self.right != None:
-			self.sceneRight = QGraphicsSceneCustom(self.graphicsView_right,name='right',model=self.model_right)
+			self.sceneRight = QtCustom.QGraphicsSceneCustom(self.graphicsView_right,name='right',model=self.model_right)
 			## set pen color yellow
 			self.sceneRight.pen = QtGui.QPen(QtCore.Qt.yellow)
 			## Load image and assign to scene
@@ -409,60 +257,13 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 			angle = angle+360
 		return angle
 
-																	## END ##
-												####### Image initialization and rotation #######
-												#################################################
+												##################### END #####################
+												###### Image initialization and rotation ######
+												###############################################
 
-												#################################################
-												#######            Update items           #######
-																	## START ##
-	def updateItems(self,model,scene):
-		row = 0
-		for item in scene.items():
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				item.setPos(float(model.data(model.index(row, 0)).toString()),float(model.data(model.index(row, 1)).toString()))
-				row += 1
-
-	def showSelectedItem(self,tableview,scene):
-		indices = tableview.selectedIndexes()
-		## Color all circles red and nly get ellipses, not text, to iterate through in green coloring process.
-		activeitems = []
-		for item in scene.items():
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				item.setPen(scene.pen)
-				activeitems.append(item)
-		## Color selected items green
-		if indices:
-			## Filter selected rows
-			rows = set(index.row() for index in indices)
-			## Paint selected rows green
-			for row in rows:
-				activeitems[row].setPen(QtGui.QPen(QtCore.Qt.green))
-
-	def deleteItem(self,tableview,model,scene):
-		indices = tableview.selectedIndexes()
-		## Only get ellipses, not text.
-		activeitems = []
-		for item in scene.items():
-			if isinstance(item, QtGui.QGraphicsEllipseItem):
-				activeitems.append(item)
-		## Deleting selected
-		if indices:
-			## Filter selected rows
-			rows = set(index.row() for index in indices)
-			## Delete selected rows in scene.
-			for row in rows:
-				scene.removeItem(activeitems[row])
-				scene.enumeratePoints()
-			scene.itemsToModel()
-
-																	## END ##
-												#######            Update items           #######
-												#################################################
-
-												#################################################
-												#######    Image processing functions     #######
-																	## START ##
+												###############################################
+												######    Image processing functions     ######
+												#################### START ####################
 	## Read image
 	def imread(self,path,normalize=True):
 		if self.debug == True: print clrmsg.DEBUG + "===== imread"
@@ -619,9 +420,9 @@ class MainWidget(QtGui.QWidget, Ui_WidgetWindow):
 		else:
 			print clrmsg.ERROR + "I'm sorry, I don't know this image shape: {0}".format(img.shape)
 
-																	## END ##
-												#######    Image processing functions    ########
-												#################################################
+												##################### END #####################
+												######    Image processing functions    #######
+												###############################################
 
 if __name__ == "__main__":
 	app = QtGui.QApplication(sys.argv)
