a789e8e5872ee50a7e9af43662547e05211284f2
Jan Arnold <jan.arnold@coraxx.net>
2016-10-27 00:08:06 +0200

    - revison number updated

--- tdct/test/test_beadPos.py
+++ tdct/test/test_beadPos.py
@@ -13,7 +13,7 @@
 # @Credits			:
 # @Maintainer		: Jan Arnold
 # @Date				: 2016/09
-# @Version			: 3DCT 2.2.2b module rev. 1
+# @Version			: 3DCT 2.2.2b module rev. 2
 # @Status			: stable
 # @Usage			: pytest
 # @Notes			:
