b1f24fe3a7e4c70a0e2a7048ec60d7e9fd4c2ec2
Jan Arnold <jan.arnold@coraxx.net>
2017-02-20 16:46:58 +0100

    - assert adjustment - added debug information print in travis config

--- tdct/test/test_parabolic.py
+++ tdct/test/test_parabolic.py
@@ -26,9 +26,15 @@ from numpy import argmax
 
 def test_parabolic():
 	f = [2, 3, 1, 6, 4, 2, 3, 1]
-	assert parabolic.parabolic(f, argmax(f)) == (3.2142857142857144, 6.1607142857142856)
+	retVal = parabolic.parabolic(f, argmax(f))
+	# assert parabolic.parabolic(f, argmax(f)) == (3.2142857142857144, 6.1607142857142856)
+	assert abs(retVal[0] - 3.2142857142857144) < 0.0001
+	assert abs(retVal[1] - 6.1607142857142856) < 0.0001
 
 
 def test_parabolic_polyfit():
 	f = [2, 3, 1, 6, 4, 2, 3, 1]
-	assert parabolic.parabolic_polyfit(f, argmax(f), 2) == (3.2142857142857295, 6.1607142857143131)
+	retVal = parabolic.parabolic_polyfit(f, argmax(f), 2)
+	# assert parabolic.parabolic_polyfit(f, argmax(f), 2) == (3.2142857142857295, 6.1607142857143131)
+	assert abs(retVal[0] - 3.2142857142857295) < 0.0001
+	assert abs(retVal[1] - 6.1607142857143131) < 0.0001
